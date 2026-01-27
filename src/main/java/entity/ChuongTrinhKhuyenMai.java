554f8f254b84c16498764b9fb154b21bf17db0bd
Tan Tung <tantung2310@gmail.com>
2026-01-27 13:17:52 +0700

    fix entity: ctpn, ctkm, lsnp, ncc, pnh

--- src/main/java/entity/ChuongTrinhKhuyenMai.java
+++ src/main/java/entity/ChuongTrinhKhuyenMai.java
@@ -102,4 +102,5 @@ public class ChuongTrinhKhuyenMai {
                 ", trangThai=" + trangThai +
                 '}';
     }
+
 }
--- src/main/java/entity/NhaCungCap.java
+++ src/main/java/entity/NhaCungCap.java
@@ -90,4 +90,5 @@ public class NhaCungCap {
                 ", trangThai=" + trangThai +
                 '}';
     }
+
 }
